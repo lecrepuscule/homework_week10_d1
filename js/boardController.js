ed8c6fa3a5cd46c05e35b305983ebc79425d8968
Yao Yuan <opal_obsidian@hotmail.com>
2015-10-12 22:39:33 +0100

    check connection function is working, but cannot recognise winner

--- js/boardController.js
+++ js/boardController.js
@@ -19,9 +19,84 @@ function BoardController(){
 
   this.markSquare = function(rowIndex, columnIndex){
     this.board[rowIndex][columnIndex] = this.currentPlayer;
-    this.currentPlayer = this.currentPlayer === "x" ? "o" : "x";
-    console.log(this.currentPlayer);
+    this.checkConnection(rowIndex, columnIndex);
+  };
+
+  this.countConnections = function(row, rIncrement, column, cIncrement, currentCount) {
+    if (this.board[row+rIncrement][column+cIncrement] === this.currentPlayer) { 
+      currentCount.push([row+rIncrement,column+cIncrement])
+    } 
+    else {
+      currentCount = [];
+    }
+    return currentCount;
   }
 
+  this.checkConnection = function(row, column) {
+    var horizontalConnections=[];
+    var verticalConnections=[];
+    var leadDiagConnections=[];
+    var antiDiagConnections=[];
+    var winner;
+
+    for (i=1-this.winCondition; i<this.winCondition; i++) {
+      if (column+i >= 0 && column+i < this.maxColumn) {
+        horizontalConnections = this.countConnections(row, 0, column, i, horizontalConnections);
+        if (row+i >= 0 && row+i < this.maxRow) {
+          leadDiagConnections = this.countConnections(row, i, column, i, leadDiagConnections);
+        }
+        if (row-i >= 0 && row-i < this.maxRow) {
+          antiDiagConnections = this.countConnections(row, -i, column, i, antiDiagConnections);
+        }
+      }
+
+      if (row+i >= 0 && row+i < this.maxRow) {
+        verticalConnections = this.countConnections(row, i, column, 0, verticalConnections);
+      }
+
+      console.log(horizontalConnections);
+      console.log(verticalConnections);
+      console.log(leadDiagConnections);
+      console.log(antiDiagConnections);
+
+      var winner = this.findWinner([horizontalConnections, verticalConnections, leadDiagConnections, antiDiagConnections]);
+      
+      if (winner) {
+        alert("we have a winner!");
+        return winner;
+      }
+      else {
+        continue;
+      }
+    }
+    // if (document.getElementsByClassName("empty").length < 1) {
+    //     return winner = "draw";
+    // }
+    this.currentPlayer = this.currentPlayer === "x" ? "o" : "x";
+  }
+
+  this.findWinner = function(connections) {
+    var winner;
+    connections.forEach(function(value){
+      debugger;
+      if (value.length >= this.winCondition) {
+        value.forEach ( function(winSpot) {
+          this.board[winSpot[0]][winSpot[1]] = this.currentPlayer + "wins";
+        })
+        winner = this.currentPlayer;
+      } 
+    })
+    return winner;
+  }
+
+  this.endGame = function(winner) {
+    alert(winner === "draw" ? "It's a draw!" : winner + " has won!");
+    if (window.confirm("Would you like to play another game?")) {
+      runGame();
+    } 
+    else {
+      alert("Bye!");
+    }
+  }
 
 }
